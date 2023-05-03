0550306efc6f3a33a70868b00189dec7519952a1
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-05-03 20:19:45 -0300

    up

--- Exercicios/tuplas/#2.py
+++ Exercicios/tuplas/#2.py
@@ -3,6 +3,7 @@ import random
 numeros = random.sample(range(1, 11), 5)
 t_numeros = tuple(numeros)
 t_ordenada = sorted(t_numeros)
-print(f'Números sorteados {t_numeros}')
+numeros_str = map(str, t_numeros)
+print(f'Números sorteados: {" - ".join(numeros_str)}')
 print(f'O menor numero sorteado foi {t_ordenada[0]}')
 print(f'O maior numero sorteado foi {t_ordenada[-1]}')
