7ccef783c943e3bba46f0d1d6908aa3f2524e5ea
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-05-03 18:17:01 -0300

    up

--- Exercicios/tuplas/#0.py
+++ Exercicios/tuplas/#0.py
@@ -1,5 +1,11 @@
 tupla = 'Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze'
-numero = int(input('Digite um numero entre 0 e 20: '))
-while numero < 0 or numero > 20:
-  numero = int(input('Inválido. Digite um numero entre 0 e 20: '))
-print(f'Você digitou o número {tupla[numero]}')
+
+while True:
+  numero = int(input('Digite um numero entre 0 e 20: '))
+  while numero < 0 or numero > 20:
+    numero = int(input('Inválido. Digite um numero entre 0 e 20: '))
+  print(f'Você digitou o número {tupla[numero]}')
+  cont = str(input('Quer continuar S/N?: ')).lower()
+  if cont not in 'Ss':
+    break
+print('Obrigada volte sempre')
